426de8fa0ee9b7fbed5adbb1d488f200aa124ea1
sunilkahalekar <kahalekar.sunil@gmail.com>
2023-12-28 16:39:20 +0530

    second commit

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+# Q&A Chatbot
+#from langchain.llms import OpenAI
+
 from dotenv import load_dotenv
 
 # To load all environment variable from .env file
@@ -5,23 +8,31 @@ load_dotenv()
 
 import streamlit as st
 import os
+import pathlib
 from PIL import Image
+import textwrap
+
 import google.generativeai as genai 
 
 # configure api key
+os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-# Function to load Gemini Pro Vision
-model=genai.GenerativeModel('gemini-pro-vision')
+
+
+
+## Function to load OpenAI model and get respones
 
 def get_gemini_response(input,image,prompt):
+    model=genai.GenerativeModel('gemini-pro-vision')
     response=model.generate_content([input,image[0],prompt])
     return response.text
 
 # input: whaterver input i have. This input basically is what is assistant to do. In this case it act as invoice extractor.
 # This prompt means 
 
-def input_image_setup(uploaded_file):
+def input_image_details(uploaded_file):
+    # Check if a file has been uploaded
     if uploaded_file is not None:
         # Read the file into bytes
         bytes_data=uploaded_file.getvalue()
@@ -34,6 +45,7 @@ def input_image_setup(uploaded_file):
         return image_parts
     else:
         raise FileNotFoundError("No file uploaded")
+
 
 # initilize  streamlit app
 st.set_page_config(page_title="Multilanguage Invoice Extractor Demo")
@@ -53,5 +65,7 @@ You are expert in understanding invoices. We will upload a image as invoice and 
 
 # If submit button is clicked
 if submit:
-    image_data=
-
+    image_data=input_image_details(uploaded_file)
+    response=get_gemini_response(input_prompt,image_data,input)
+    st.subheader("The response is")
+    st.write(response)
